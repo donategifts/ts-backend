d59fc613415a8fa4dcfa0057490ca64e60114a5a
gnorbsl <ph@mycraftnote.de>
2022-09-16 09:33:44 +0200

    test stuff

---
+++ src/controllers/IndexController.ts
@@ -0,0 +1,14 @@
+import {controller, httpGet, interfaces, next, request, response} from "inversify-express-utils";
+import * as express from "express";
+
+@controller("/")
+export class IndexController implements interfaces.Controller {
+	@httpGet("/health")
+	private async getHealth(
+		@request() _req: express.Request,
+		@response() _res: express.Response,
+		@next() _next: express.NextFunction,
+	): Promise<string> {
+		return "I'M ALIVE; KNEEL DOWN, IT IS I YOUR LORD AND SAVIOR";
+	}
+}
