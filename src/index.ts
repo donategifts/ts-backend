378f1933121d06a2b285cbe13581a4bf4d1f7893
gnorbsl <ph@mycraftnote.de>
2022-09-18 07:37:22 +0200

    added host env

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import {server} from "./core/server";
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
-server.build().listen(parseInt(port as string), host, () => console.log(`Listen on http://localhost:${port}/`));
+server.build().listen(parseInt(port as string), host, () => console.log(`Listen on http://${host}:${port}/`));
 
